74fd14c3f48dfba86eb59b2f2beb046e2a6a8ff2
Achuth <armorachyuth123@gmail.com>
2020-07-28 16:08:26 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,10 +12,11 @@ export default function Home() {
             <script async src="https://www.googletagmanager.com/gtag/js?id=UA-169182524-1"></script>
                 <script>
                   window.dataLayer = window.dataLayer || [];
-                  function gtag(){dataLayer.push(arguments);}
+                  function gtag(){dataLayer.push(arguments)}
                   gtag('js', new Date());
 
                   gtag('config', 'UA-169182524-1');
+    
                 </script>   
 
             </Head>
