a5bfbf64d500a597cabfc554fa11892001c1c1a1
Achuth <armorachyuth123@gmail.com>
2020-08-27 13:01:41 +0530

    fixed gtag

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
             <script async src="https://www.googletagmanager.com/gtag/js?id=UA-169182524-1"></script>
                 <script>
                   window.dataLayer = window.dataLayer || [];
-                  function gtag(){dataLayer.push(arguments)}
+                  function gtag(){window.dataLayer.push(arguments)}
                   gtag('js', new Date());
 
                   gtag('config', 'UA-169182524-1');
