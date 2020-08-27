9f30c48c436fe17b611a4956611d34658d9afda1
Achuth <armorachyuth123@gmail.com>
2020-08-27 13:03:08 +0530

    removed gtag

--- pages/index.js
+++ pages/index.js
@@ -8,18 +8,7 @@ export default function Home() {
             <Head>
                 <title>Captur | simple screen recorder built for better user experience</title>
                 <link rel="icon" href="/favicon.ico" />
-        
-            <script async src="https://www.googletagmanager.com/gtag/js?id=UA-169182524-1"></script>
-                <script>
-                  window.dataLayer = window.dataLayer || [];
-                  function gtag(){window.dataLayer.push(arguments)}
-                  gtag('js', new Date());
-
-                  gtag('config', 'UA-169182524-1');
-    
-                </script>   
-
-            </Head>
+                    </Head>
 
             <main>
                 <h1>
